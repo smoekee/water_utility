31e1539d2d529012261689b6f303f51f55c6444c
Smoki <schmalzes@gmail.com>
2023-11-25 17:13:18 -0500

    water_utility_app base push

---
+++ reporter/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Incidences
+# Register your models here.
+
+class IncidencesAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+
+
+admin.site.register(Incidences, IncidencesAdmin)
