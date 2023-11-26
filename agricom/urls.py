10f51a4cdf2a814f960784bad93719034efc2df2
Smoki <schmalzes@gmail.com>
2023-11-25 23:19:05 -0500

    added data endpoints in urls

--- agricom/urls.py
+++ agricom/urls.py
@@ -20,8 +20,11 @@ from django.urls import path, re_path
 from reporter.models import WaterConsumption
 
 import reporter.views
+from reporter.views import waterconsumption_dataset, top10_consumers
 
 urlpatterns = [
     re_path(r'^$', reporter.views.home, name='home'),
+    re_path(r'^waterconsumption_data/$', waterconsumption_dataset, name='waterconsumption'),
+    re_path(r'^top10_consumers/$', top10_consumers, name='top10_consumers'),
     path('admin/', admin.site.urls)
 ]
