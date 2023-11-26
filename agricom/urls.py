0597de67e1c681bae14a101e37abc8aff7ba42e5
Smoki <schmalzes@gmail.com>
2023-11-25 22:16:38 -0500

    updated homepage

--- agricom/urls.py
+++ agricom/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from reporter.models import WaterConsumption
+
+import reporter.views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^$', reporter.views.home, name='home'),
+    path('admin/', admin.site.urls)
 ]
