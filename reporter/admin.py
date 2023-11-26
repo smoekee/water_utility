24c1de7adf6ff7acc7a416acfbcee316c05800e3
Smoki <schmalzes@gmail.com>
2023-11-25 21:03:57 -0500

    adfa

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Incidences
+from django.contrib.gis.geos import Point
+from datetime import datetime
+from leaflet.admin import LeafletGeoAdmin
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+from reporter.models import WaterConsumption
+
 # Register your models here.
 
 class IncidencesAdmin(admin.ModelAdmin):
@@ -7,3 +16,32 @@ class IncidencesAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Incidences, IncidencesAdmin)
+
+class WaterConsumptionAdmin(LeafletGeoAdmin):
+    pass
+
+admin.site.register(WaterConsumption, WaterConsumptionAdmin)
+
+
+df_excelReader = pd.read_excel("/home/smoki/geodjango/agricom/waterwatch_clean2.xlsx")
+
+# print(df_excelReader.head())
+
+for index, row in df_excelReader.iterrows():
+    Id = index
+    Suburb = row['Suburb']
+    NoOfSingleResProp = row['Number of single-residential properties_number']
+    AvgMonthlyKL = row['Oct 2017\nkl/month']
+    AvgMonthlyKLPredicted = 0
+    PredictionAccuracy = 0
+    Month = row['Month']
+    Year = row['Yearadf']
+    DateTime = datetime.now()
+    Longitude = row['Longitude']
+    Latitude = row['Latitude']
+
+
+    WaterConsumption(Id=Id, Suburb=Suburb,NoOfSingleResProp = NoOfSingleResProp,
+                     AvgMonthlyKL=AvgMonthlyKL, AvgMonthlyKLPredicted=AvgMonthlyKLPredicted,
+                     PredictionAccuracy=PredictionAccuracy, Month=Month, Year=Year,
+                     DateTime=DateTime, geom=Point(Longitude, Latitude)).save()
--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,31 @@
-# from django.db import models
+#from django.db import models
 from django.contrib.gis.db import models
+
 # Create your models here.
+class Incidences(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural =" Incidences" 
+
+class WaterConsumption(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    Suburb = models.CharField(max_length=100)
+    NoOfSingleResProp = models.IntegerField()
+    AvgMonthlyKL = models.IntegerField()
+    AvgMonthlyKLPredicted = models.IntegerField()
+    PredictionAccuracy = models.IntegerField()
+    Month = models.CharField(max_length=50)
+    Year = models.IntegerField()
+    DateTime = models.DateTimeField()
+    geom = models.PointField()
+
+    def __str__(self):
+        return self.Suburb
+
+    class Meta:
+        verbose_name_plural = 'WaterConsumption'
